3aa7c97e99dc3aa3fc74a4f4176e479ad661d8d2
QingYu-NEU <yu.qing4@northeastern.edu>
2023-07-31 21:04:22 -0700

    basic class framework finished

---
+++ src/application/Delivery.java
@@ -0,0 +1,57 @@
+package application;
+
+public class Delivery {
+	private String trackingNumber;
+	private String recipientName;
+	private boolean isSigned;
+	
+	public Delivery(String trackingNumber, String recipientName) {
+        this.trackingNumber = trackingNumber;
+        this.recipientName = recipientName;
+        this.isSigned = false;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public Boolean isSigned() {
+        return isSigned;
+    }
+
+    public void setSigned(boolean signed) {
+        this.isSigned = signed;
+    }
+    
+    @Override
+    public String toString() {
+        return "Tracking Number: " + trackingNumber + ",\nRecipient: " + recipientName + ",\nSigned: " + isSigned;
+    }
+//    
+//    public void printDeliveryInfo() {
+//    	System.out.println("Tracking Number: " + trackingNumber + ",\nRecipient: " + recipientName + ",\nSigned: " + isSigned);
+//    }
+}
+
+//1. 还可以加什么method
+//2. SceneBuilder -- java event-trigger
+//3. 加功能：method（1）， 运转起来（2）
+
+
+
+
+
+    
+
---
+++ src/application/Notification.java
@@ -0,0 +1,31 @@
+package application;
+
+public class Notification {
+    private Recipient recipient;
+    private String message;
+    private boolean isRead;
+
+    // Constructor
+    public Notification(Recipient user, String message) {
+        this.recipient = user;
+        this.message = message;
+        this.isRead = false;
+    }
+
+    // Getters and Setters
+    public Recipient getUser() {
+        return recipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+}
