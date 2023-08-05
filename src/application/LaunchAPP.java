0b143ef6f0b70e1bd3167dfce007342e44cf7ba5
QingYu-NEU <yu.qing4@northeastern.edu>
2023-08-04 18:13:47 -0700

    set CarrLoginView.fxml

--- src/application/LaunchAPP.java
+++ src/application/LaunchAPP.java
@@ -1,19 +1,14 @@
 package application;
 
-import java.io.IOException;
-
-import application.forget.*;
+//import application.forget.*;
 import application.model.*;
 import application.login.*;
-import application.regist.*;
+//import application.regist.*;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 
 public class LaunchAPP extends Application {
@@ -24,112 +19,91 @@ public class LaunchAPP extends Application {
 	private Recipient recipient;
 	
 	@Override
-	public void start(Stage primaryStage){
-		try {
-			stage = primaryStage;
-			showLoginView();
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}			
+	public void start(Stage primaryStage) throws Exception{
+		stage = primaryStage;
+		showLoginView();
+		primaryStage.show();		
 	}
 	
 	// show login view
-	public void showLoginView() {
-		try {
-			stage.setTitle("Login");
-			stage.getIcons().clear();
-			LoginViewController lgController = (LoginViewController)replaceSceneContent("appication.login/LoginView.fxml");
-			lgController.setLaunchApp(this);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void showLoginView() throws Exception {
+		stage.setTitle("Delivery Management System");
+		stage.getIcons().clear();
+		LoginViewController lgController = (LoginViewController)replaceSceneContent("login/LoginView.fxml");
+		lgController.setLaunchApp(this);
 	}
 	
-	public void showCarrLoginView() {
-		try {
-			stage.setTitle("Carrier Login");
-			stage.getIcons().clear();
-			CarrLoginViewController carrlgController = (CarrLoginViewController)replaceSceneContent("login/CarrLoginView.fxml");
-			carrlgController.setLaunchApp(this);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void showCarrLoginView() throws Exception {
+		stage.setTitle("Carrier Login");
+		stage.getIcons().clear();
+		CarrLoginViewController carrlgController = (CarrLoginViewController)replaceSceneContent("login/CarrLoginView.fxml");
+		carrlgController.setLaunchApp(this);
 	}
 	
-	public void showReciLoginView() {
-		try {
-			stage.setTitle("Login");
-			stage.getIcons().clear();
-			stage.getIcons().add(new Image("file:images/login.png"));
-			LoginViewController lgController = (LoginViewController)replaceSceneContent("login/LoginView.fxml");
-			lgController.setLaunchApp(this);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void showReciLoginView() throws Exception {
+		stage.setTitle("Recipient Login");
+		stage.getIcons().clear();
+		ReciLoginViewController recilgController = (ReciLoginViewController)replaceSceneContent("login/ReciLoginView.fxml");
+		recilgController.setLaunchApp(this);
 	}
 	
 	// show forget view
-	public void showForgetView() {
-		try {
-			stage.setTitle("Forget");
-			ForgetViewController fgController = (ForgetViewController)replaceSceneContent("forget/ForgetView.fxml");
-			fgController.setLaunchApp(this);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public void showForgetView() {
+//		try {
+//			stage.setTitle("Forget");
+//			ForgetViewController fgController = (ForgetViewController)replaceSceneContent("forget/ForgetView.fxml");
+//			fgController.setLaunchApp(this);
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
 	// show reset password view
-	public void showResetPasswdView() {
-		try {
-			stage.setTitle("ResetPasswdView");
-			ResetPasswdViewController rpController = (ResetPasswdViewController)replaceSceneContent("forget/ResetPasswdView.fxml");
-			rpController.setLaunchApp(this);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public void showResetPasswdView() {
+//		try {
+//			stage.setTitle("ResetPasswdView");
+//			ResetPasswdViewController rpController = (ResetPasswdViewController)replaceSceneContent("forget/ResetPasswdView.fxml");
+//			rpController.setLaunchApp(this);
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
 	// show registration view
-	public void showRegistView() {
-		try {
-			stage.setTitle("Regist");
-			stage.getIcons().add(new Image("file:images/regist.png"));
-			
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(LunchApp.class.getResource("regist/RegistView.fxml"));
-			BorderPane bp = (BorderPane)loader.load();
-			scene = new Scene(bp);
-			stage.setScene(scene);
-			stage.setResizable(false);
-			
-			RegistViewController regController = (RegistViewController)loader.getController();
-			System.out.println(regController);
-			regController.setLunchApp(this);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public void showRegistView() {
+//		try {
+//			stage.setTitle("Regist");
+//			stage.getIcons().add(new Image("file:images/regist.png"));
+//			
+//			FXMLLoader loader = new FXMLLoader();
+//			loader.setLocation(LunchApp.class.getResource("regist/RegistView.fxml"));
+//			BorderPane bp = (BorderPane)loader.load();
+//			scene = new Scene(bp);
+//			stage.setScene(scene);
+//			stage.setResizable(false);
+//			
+//			RegistViewController regController = (RegistViewController)loader.getController();
+//			System.out.println(regController);
+//			regController.setLunchApp(this);
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
 	// replace scene content
-	private Object replaceSceneContent(String fxmlFile) {
+	private Object replaceSceneContent(String fxmlFile) throws Exception {
+		
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(LaunchApp.class.getResource(fxmlFile));
-		
-		AnchorPane ap = null;
-		try {
-			ap = (AnchorPane)loader.load();
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-		
+		loader.setLocation(getClass().getResource(fxmlFile));
+		AnchorPane ap = (AnchorPane)loader.load();
+
 		scene = new Scene(ap);
 		stage.setScene(scene);
 		stage.setResizable(false);
 		
 		return loader.getController();
 	}
+
 	
 	public Scene getScene() {
 		return scene;
