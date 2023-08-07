57394c4638dde610a82020e626f1cee3794796b7
QingYu-NEU <yu.qing4@northeastern.edu>
2023-08-07 14:54:36 -0700

    finish login, forget, regist UI

--- src/application/login/CarrLoginViewController.java
+++ src/application/login/CarrLoginViewController.java
@@ -4,33 +4,34 @@ import application.model.*;
 import application.system.Controller;
 import application.tools.*;
 
-import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.util.Duration;
 import javafx.scene.control.TextField;
 
 public class CarrLoginViewController extends Controller{
 
-    @FXML
+	@FXML
     private Button btnCarriRegister;
 
     @FXML
     private Button btnForget;
 
     @FXML
+    private Button btnHome;
+
+    @FXML
     private Button btnLogin;
-    
+
     @FXML
     private Button btnReturn;
-    
+
     @FXML
     private TextField passwdInput;
 
     @FXML
     private TextField userNameInput;
-
+    
     @FXML
     void btnCarriRegisterClick(ActionEvent event) throws Exception {
     	launchApp.showRegistView();
@@ -43,30 +44,13 @@ public class CarrLoginViewController extends Controller{
 
     @FXML
     void btnLoginClick(ActionEvent event) throws Exception {
-    	launchApp.showCarrierMainView();
-    	//    	String userName = userNameInput.getText();
-//		String password = passwdInput.getText();
-//		
-//		if(UserPasswdCheck.isValidUserName(userName) && UserPasswdCheck.isValidPassword(password)) {
-//			Carrier carrier = Database.getCarrier(userName, password);
-//			if(carrier != null) {
-//				launchApp.setCarrier(carrier);
-//				launchApp.showCarrierMainView();
-//			}else {
-//				userNameInput.clear();
-//				passwdInput.clear();
-//			}
-//		}else {
-//			userNameInput.clear();
-//			passwdInput.clear();
-//		}
+
     }
-    
+
     @FXML
     void btnReturnClick(ActionEvent event) throws Exception {
     	launchApp.showMainView();
     }
-
 }
 
 
