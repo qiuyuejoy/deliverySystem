e410974878c4726f2f92c000aa726144ee5c571e
qiuyuejoy <122427448+qiuyuejoy@users.noreply.github.com>
2023-08-06 22:22:50 -0700

    carrierUpdate

---
+++ src/application/carrier/CarrierChangeStatusViewController.java
@@ -0,0 +1,25 @@
+package application.carrier;
+
+import javafx.event.ActionEvent;
+
+import appication.system.Controller;
+import application.model.Carrier;
+import application.model.Delivery;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+public class CarrierChangeStatusViewController extends Controller{
+	
+	
+	
+    @FXML
+    private Button btnArrived;
+
+    @FXML
+    void ArrivedClick(ActionEvent event) throws Exception {
+
+    }
+       
+}
